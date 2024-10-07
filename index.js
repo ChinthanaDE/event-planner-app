563467b8854e8cf28f6ee4eacab38abedb1ff2a5
Chinthana Desilva <chinthanadesilva2@gmail.com>
2024-10-07 22:07:00 +0530

    Revert "Feature/user profile creation"

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import { AppRegistry } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
-const MainApp = () => (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-    <App />
-  </GestureHandlerRootView>
-);
-
-AppRegistry.registerComponent(appName, () => MainApp);
+AppRegistry.registerComponent(appName, () => App);
