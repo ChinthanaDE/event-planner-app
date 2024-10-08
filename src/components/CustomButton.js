1d7c033fbd441f7187c63cf17121693b75af2000
Chinthana Desilva <chinthanadesilva2@gmail.com>
2024-10-08 23:02:21 +0530

    refactor: custom button add styles

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
 
-const CustomButton = ({ title, onPress, buttonStyle, textStyle }) => (
-  <TouchableOpacity style={[styles.button, buttonStyle]} onPress={onPress}>
-    <Text style={[styles.text, textStyle]}>{title}</Text>
+const CustomButton = ({
+  title,
+  onPress,
+  buttonStyle,
+  textStyle,
+  type = 'primary',
+}) => (
+  <TouchableOpacity
+    style={[styles.button, styles[`${type}Button`], buttonStyle]}
+    onPress={onPress}>
+    <Text style={[styles.text, styles[`${type}ButtonText`], textStyle]}>
+      {title}
+    </Text>
   </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
-    button: {
-      width: '100%',
-      backgroundColor: '#E86C4F',
-      paddingVertical: 16,
-      borderRadius: 8,
-      alignItems: 'center',
-      marginTop: 16,
-    },
-    text: {
-      color: '#fff',
-      fontSize: 16,
-      fontWeight: '600',
-    },
-  });
+  button: {
+    borderRadius: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    alignItems: 'center',
+    marginBottom: 12,
+  },
+  primaryButton: {
+    backgroundColor: '#E97451',
+  },
+  secondaryButton: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#E97451',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  primaryButtonText: {
+    color: 'white',
+  },
+  secondaryButtonText: {
+    color: '#E97451',
+  },
+});
 
 export default CustomButton;
