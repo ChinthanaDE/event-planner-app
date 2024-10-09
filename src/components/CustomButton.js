dad039dba650bfdfdba86364a44bc2572dc3ed73
Chinthana Desilva <chinthanadesilva2@gmail.com>
2024-10-09 05:37:41 +0530

    refactor: add left icon  rigth icon to custom button

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const CustomButton = ({
   title,
@@ -7,31 +8,48 @@ const CustomButton = ({
   buttonStyle,
   textStyle,
   type = 'primary',
+  showLeftIcon, 
+  showRightIcon,
 }) => (
   <TouchableOpacity
     style={[styles.button, styles[`${type}Button`], buttonStyle]}
     onPress={onPress}>
+    {showLeftIcon && (
+      <Icon
+        name="arrow-back"
+        size={20}
+        color={styles[`${type}ButtonText`].color}
+        style={styles.iconLeft}
+      />
+    )}
     <Text style={[styles.text, styles[`${type}ButtonText`], textStyle]}>
       {title}
     </Text>
+    {showRightIcon && (
+      <Icon
+        name="arrow-forward" 
+        size={20}
+        color={styles[`${type}ButtonText`].color}
+        style={styles.iconRight}
+      />
+    )}
   </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
   button: {
-    borderRadius: 8,
     paddingVertical: 12,
     paddingHorizontal: 24,
     alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
     marginBottom: 12,
   },
   primaryButton: {
     backgroundColor: '#E97451',
   },
   secondaryButton: {
-    backgroundColor: 'white',
-    borderWidth: 1,
-    borderColor: '#E97451',
+    backgroundColor: 'rgba(218, 94, 66, 0.08)',
   },
   buttonText: {
     fontSize: 16,
@@ -43,6 +61,13 @@ const styles = StyleSheet.create({
   secondaryButtonText: {
     color: '#E97451',
   },
+  iconLeft: {
+    marginRight: 8,
+  },
+  iconRight: {
+    marginLeft: 8,
+  },
 });
 
 export default CustomButton;
+
