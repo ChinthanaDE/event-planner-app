cd4939d4346a2c105b71c012ee32358d220da0a4
Chinthana Desilva <chinthanadesilva2@gmail.com>
2024-10-12 20:16:34 +0530

    fix: remove deleted

--- src/components/skeletons/EventHeaderSkeleton.js
+++ src/components/skeletons/EventHeaderSkeleton.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Dimensions } from 'react-native';
+import {View, Dimensions} from 'react-native';
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
-const { width } = Dimensions.get('window');
+const {width} = Dimensions.get('window');
 
 const EventHeaderSkeleton = () => (
   <SkeletonPlaceholder>
-    <View style={{ width, height: 300 }} />
+    <View style={{width, height: 300}} />
   </SkeletonPlaceholder>
 );
 
