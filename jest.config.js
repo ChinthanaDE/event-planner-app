80ae203e7dd00c81fa59efa1d6a3b8f567704959
Chinthana Desilva <chinthanadesilva2@gmail.com>
2024-10-14 19:07:15 +0530

    feat: jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,22 @@
+// jest.config.js
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-navigation|@react-native-community|react-native-vector-icons|react-redux)/)',
+  ],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/android/',
+    '<rootDir>/ios/',
+  ],
+  testMatch: ['**/__tests__/**/*.test.js'],
 };
