fcac62363f7a655eb50f8641b236eb8152421b0e
Chinthana Desilva <chinthanadesilva2@gmail.com>
2024-10-15 10:33:01 +0530

    refactor: add comment in authSlice

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -3,6 +3,20 @@ import {auth, firestore, storage} from '../../utils/firebaseConfig';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {getErrorMessage} from '../../utils/firebaseError';
 
+/**
+ * Initial state for the authentication slice.
+ *
+ * @typedef {Object} AuthState
+ * @property {Object|null} user - The currently authenticated user object or `null` if not authenticated.
+ * @property {boolean} isAuthenticated - A flag indicating whether the user is authenticated (`true`) or not (`false`).
+ * @property {boolean} isLoading - A flag representing whether an authentication operation (login/signup) is in progress (`true`).
+ * @property {string|null} error - The error message from an authentication operation or `null` if no error occurred.
+ * @property {number} registrationStep - A number representing the current step in the registration process (0: initial, 1: image upload, 2: completed).
+ * @property {string|null} profileImage - The local URI of the user's profile image, or `null` if no image is set.
+ * @property {string|null} profileImageUrl - The remote URL of the user's profile image, or `null` if not uploaded.
+ * @property {Object|null} personalInfo - An object containing the user's personal information or `null` if not available.
+ * @property {boolean} hasCompletedRegistration - A flag indicating whether the user has completed the registration process (`true`) or not (`false`).
+ */
 const initialState = {
   user: null,
   isAuthenticated: false,
@@ -15,6 +29,11 @@ const initialState = {
   hasCompletedRegistration: false,
 };
 
+/**
+ * Authentication slice manage authentication-related state and actions.
+ *
+ * @type {Slice<AuthState>}
+ */
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -77,7 +96,11 @@ const handleError = (error, dispatch) => {
   dispatch(setError(errorMessage));
   return false;
 };
-
+/**
+ * Checks the current authentication state of the user.
+ *
+ * @returns {Function} - checks if the user is currently authenticated.
+ */
 export const checkAuthState = () => async dispatch => {
   dispatch(setLoading(true));
   try {
@@ -129,7 +152,13 @@ export const checkAuthState = () => async dispatch => {
     dispatch(setLoading(false));
   }
 };
-
+/**
+ * Logs in a user with their email and password.
+ *
+ * @param {string} email - The user's email address.
+ * @param {string} password - The user's password.
+ * @returns {Function} - perform the login process.
+ */
 export const login = (email, password) => async dispatch => {
   dispatch(setLoading(true));
   dispatch(clearError());
@@ -175,7 +204,13 @@ export const login = (email, password) => async dispatch => {
     dispatch(setLoading(false));
   }
 };
-
+/**
+ * Signs up a new user with email and password.
+ *
+ * @param {string} email - The user's email address.
+ * @param {string} password - The user's password.
+ * @returns {Function} -  perform the signup process.
+ */
 export const signup = (email, password) => async dispatch => {
   dispatch(setLoading(true));
   dispatch(clearError());
@@ -204,6 +239,11 @@ export const signup = (email, password) => async dispatch => {
   }
 };
 
+/**
+ * Logs out the currently authenticated user.
+ *
+ * @returns {Function} - perform the logout process.
+ */
 export const logoutUser = () => async dispatch => {
   dispatch(setLoading(true));
   try {
@@ -217,6 +257,16 @@ export const logoutUser = () => async dispatch => {
     dispatch(setLoading(false));
   }
 };
+
+/**
+ * Uploads the user's profile image and updates the state with the image URLs.
+ * 
+ * This function uploads the image from a local URI to cloud storage and updates
+ * the corresponding document in Firestore with the new profile image URL.
+ * 
+ * @param {string} imageUri - The local URI of the image to be uploaded.
+ * @returns {Function} - handle the image upload process.
+ */
 
 export const submitProfileImage = imageUri => async (dispatch, getState) => {
   dispatch(setLoading(true));
@@ -267,6 +317,15 @@ export const submitProfileImage = imageUri => async (dispatch, getState) => {
   }
 };
 
+/**
+ * Submits the user's personal information and updates it in Firestore and Redux state.
+ * 
+ * This function updates the user's personal information such as name, phone, address,
+ * and marks the registration process as completed.
+ * 
+ * @param {Object} personalInfo - The user's personal information (e.g., firstName, lastName, phone, etc.).
+ * @returns {Function} - handle the personal information submission process.
+ */
 export const submitPersonalInfo =
   personalInfo => async (dispatch, getState) => {
     dispatch(setLoading(true));
@@ -297,6 +356,16 @@ export const submitPersonalInfo =
     }
   };
 
+/**
+ * Updates both the user's personal information and profile image in Firestore and Redux state.
+ * 
+ * This function handles the update of the user's personal information and uploads a new profile
+ * image if provided. It ensures both fields are updated and synced across Firestore and Firebase Authentication.
+ * 
+ * @param {Object} personalInfo - The user's personal information (e.g., firstName, lastName, etc.).
+ * @param {string|null} imageUri - The local URI of the image to be uploaded, or `null` if no image change.
+ * @returns {Function} - handle the combined profile update.
+ */
 export const updateProfile = newData => async (dispatch, getState) => {
   dispatch(setLoading(true));
   try {
