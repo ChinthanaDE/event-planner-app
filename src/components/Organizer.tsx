18893e235bc9fa2b35a6eb351ff8aa294ff3f016
Chinthana Desilva <chinthanadesilva2@gmail.com>
2024-10-20 10:13:25 +0530

    refactor: Organizer componnet change to tsx

--- src/components/Organizer.tsx
+++ src/components/Organizer.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  ViewStyle,
+  TextStyle,
+  ImageStyle,
+} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Organizer = ({name, email, avatarUrl}) => (
+interface OrganizerProps {
+  name: string;
+  email: string;
+  avatarUrl?: string;
+}
+
+const Organizer: React.FC<OrganizerProps> = ({name, email, avatarUrl}) => (
   <TouchableOpacity style={styles.organizerContainer}>
     {avatarUrl && <Image source={{uri: avatarUrl}} style={styles.avatar} />}
     <View style={styles.organizerInfo}>
@@ -13,7 +28,15 @@ const Organizer = ({name, email, avatarUrl}) => (
   </TouchableOpacity>
 );
 
-const styles = StyleSheet.create({
+interface Styles {
+  organizerContainer: ViewStyle;
+  avatar: ImageStyle;
+  organizerInfo: ViewStyle;
+  organizerName: TextStyle;
+  organizerEmail: TextStyle;
+}
+
+const styles = StyleSheet.create<Styles>({
   organizerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
